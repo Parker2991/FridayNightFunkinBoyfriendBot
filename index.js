faadb943a39a8d8e577ca744d9a5d8928f3a45af
Parker2991 <parkerallengriffith05@gmail.com>
2024-02-12 21:12:19 +0000

    v1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mineflayer = require('mineflayer')
 let symbol = require('illegal-symbols')
-var server = 'sus.shhnowisnottheti.me'
+var server = ''
 //NMxigAU6dR1KfgaQkKz87L
 const readline = require('readline')
 
@@ -25,8 +25,8 @@ function between(min, max) {
 }
 
 //variables
-var prefix = '&8[&b&lBOYFRIEND&8]&a[&lBeta] &8[&4&lParker2991&8]';
-var consoleprefix = 'bcraw  &8 &l &m[&4 &mParker2991&8] &8 &m[&b &mBOYFRIEND&8] &8 &m[&b &mCONSOLE&8] ';
+var prefix = '&8[&b&lFNFBoyfriendBot&8]&b[&l1.0 RELEASE!] &8[&4&lParker2991&8]';
+var consoleprefix = 'bcraw  &8 &l &m[&4 &mParker2991&8] &8 &m[&b &FNFBoyfriendBot&8] &8 &m[&b &mCONSOLE&8] ';
 
 function randomchar() {
   const crypto = require("crypto");
@@ -39,25 +39,24 @@ function randomchar() {
 }
 
 function runInCore(cmd) {
-  bot._client.write('update_command_block', { location: { x: between(Math.floor(bot.entity.position.x) + 1, Math.floor(bot.entity.position.x) - 15), y: between(0, 3), z: between(Math.floor(bot.entity.position.z) + 1, Math.floor(bot.entity.position.z) - 15) }, command: cmd, mode: 1, flags: 0b100 });
+    bot._client.write('update_command_block', {location: {x:between(Math.floor(bot.entity.position.x) + 1, Math.floor(bot.entity.position.x) - 15), y:between(0, 3), z:between(Math.floor(bot.entity.position.z) + 1, Math.floor(bot.entity.position.z) - 15)}, command: cmd, mode: 1, flags: 0b100});
 }
+
 bot.on('login', async () => { //                      time in ms
  console.log(`logged in as ${bot.username}`)
-  
+
   //change the coords if bot has problems
 //await sendChat('/tp '+require('randomstring').generate({length:5,charset:'1234567890'})+' 5 '+require('randomstring').generate({length:6,charset:'1234567890'}))
-  
+
   await sendChat('/tptoggle')
   await sendChat('/online')
-  await sendChat('/vanish')
   await sendChat('/v on')
   await sendChat('/console ')
-  await sendChat('/username  &8[&a&lBetaBot&8]')
+  await sendChat('/username &b&l[1.0 Bot]')
   await sendChat('/c')
   await sendChat('/tptoggle parker2991')
   await sendChat('/tp Parker2991')
   await sendChat('/tptoggle parker2991')
-  await sendChat('/Username ')
   await sendChat('/night')
   await sendChat('/gamerule doMobSpawning false')
 
@@ -65,18 +64,19 @@ bot.on('login', async () => { //                      time in ms
   await sendChat('/online')
   await sendChat('/cspy on')
   await sendChat('/prefix &4[BOT/CONSOLE]')
-  await sendChat('/sudo Parker2991 prefix &b &l &m[Boyfriend]')
+  await sendChat('/sudo Parker2991 prefix &b &l[Owner of FNFBoyfriendbot 1.0]')
 
- await sendChat(`/fill ${Math.floor(bot.entity.position.x)} 0 ${Math.floor(bot.entity.position.z)} ${Math.floor(bot.entity.position.x) - 15} 50 ${Math.floor(bot.entity.position.z - 15)} command_block replace`)
-  runInCore('bcraw ' + prefix + '&b &lOwner is Parker2991')
+ await sendChat(`/fill ${Math.floor(bot.entity.position.x)} 0 ${Math.floor(bot.entity.position.z)} ${Math.floor(bot.entity.position.x) - 15} 2 ${Math.floor(bot.entity.position.z - 15)} command_block replace`);
+  await sendChat('/bcraw ' + prefix + '&b &lOwner is Parker2991')
 
-  runInCore('bcraw ' + prefix + '&b &l almost complete bot its in beta as of rn the orignal code is up and working as of right now but its old code now and bot commands only work in the console as of right now im gonna try to work on that or try atleast i want to make it to work in console and in game gotta figure out some things and debug the bot and maybe a white list?')
-  runInCore('bcraw ' + prefix + 'command list is not know as this time im still trying to figure out all the code for the bot')
-  runInCore('bcraw ' + prefix + 'like always daily meme because why not')
-  runInCore('bcraw ' + prefix + 'no meme today sorry =(')
+  await sendChat('/bcraw ' + prefix + '&b&l1.0 RELEASE!!!!! 1/26/23 1:47am release date after 3 months its finally ready for 1.0 release credits to logintimedout for making the bot so i can add onto it the commands has been expanded')
+
+  await sendChat('/bcraw ' + prefix + 'command list is not know as this time im still trying to figure out all the code for the bot')
+
+
+  await sendChat('/bcraw ' + prefix + 'cocaine and crack? idk im out of ideas for memes someone give me ')
+
 })
-
-
 const cmd = require('mineflayer-cmd').plugin
 
 cmd.allowConsoleInput = false // Optional config argument
@@ -106,14 +106,12 @@ rl.on('line', (line) => {
   switch (command) {
     case 'fakekick':
       runInCore('msg ' + args + ' @e @e @e @e @e @e @e @e @e')
-      runInCore('bcraw &8&l[&b&m&lBoyfriendbot]&8 ' + args + ' has been kicked!')
+      runInCore('bcraw &8&l[&b&m&lFNFBoyfriendbot]&8 ' + args + ' has been kicked!')
      break
   }
 })
 
-rl.on('line', (line) => {
-  let args = line.split(' ')
-  let command = args.shift()
+
 
   rl.on('line', (line) => {
   let args = line.split(' ')
@@ -133,58 +131,32 @@ rl.on('line', (line) => {
   let command = args.shift()
 
   switch (command) {
-    case 'op':
-      runInCore('sudo + botusername op + botusername')
-      break
+    case 'deop':
+       setInterval(function() { runInCore('sudo + playerusername deop @s[type=player]'), 1 })
   }
 })
+rl.on('line', (line) => {
+let args = line.split(' ')
+let command = args.shift()
     switch (command) {
-        
+
       case 'kaboom':
-    runInCore('sudo  *  kaboom')  
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
-    runInCore('sudo  *  kaboom')
+    setInterval(function() { runInCore('sudo  * kaboom'), 8})
       runInCore('bcraw have fun =)')
 
     break
       }
 
 })
-  rl.on('line', (line) => {
-  let args = line.split(' ')
-  let command = args.shift()
 
 rl.on('line', (line) => {
   let args = line.split(' ')
   let command = args.shift()
 
-  
-           
+
+
 switch (command) {
-        
+
       case 'BOOM':
       runInCore('sudo  *  /fast')
     runInCore('sudo  *  gms')
@@ -196,10 +168,12 @@ runInCore('sudo  *  /sphere tnt 75')
       }
 
 })
-    
-           
+
+rl.on('line', (line) => {
+let args = line.split(' ')
+let command = args.shift()
 switch (command) {
-        
+
       case 'kick':
       runInCore('/console @e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e@e')
       runInCore('bcraw &8&l[&b&m&lBoyfriendbot]&8 ' + args + ' has been kicked!')
@@ -216,7 +190,7 @@ rl.on('line', (line) => {
   switch (command) {
     case 'greeting':
 
-      runInCore('bcraw &8&lWelcome Owner &4&l&m[Ayunami2000] To The Server!!!!!!')
+      runInCore('bcraw &e Ayunami2000 Joined the game')
 
       break
   }
@@ -374,6 +348,18 @@ rl.on('line', (line) => {
       break
   }
 })
+
+rl.on('line', (line) => {
+  let args = line.split(' ')
+  let command = args.shift()
+
+  switch (command) {
+    case 'your':
+      setInterval(function() { runInCore('gms ' + args), 1 })
+      break
+  }
+})
+
 rl.on('line', (line) => {
   let args = line.split(' ')
   let command = args.shift()
@@ -482,7 +468,7 @@ rl.on('line', (line) => {
       setInterval(function() { runInCore('give @a crafting_table'), 1 })
       setInterval(function() { runInCore('give @a diamond_block'), 1 })
       setInterval(function() { runInCore('smite *'), 1 })
-     setInterval(function() { runInCore('kaboom' + args), 1 })
+     setInterval(function() { runInCore('kaboom'), 1 })
 
       break
   }
@@ -519,10 +505,10 @@ rl.on('line', (line) => {
   let command = args.shift()
 
   switch (command) {
-    case 'deop':
+    case 'serverdeop':
       setInterval(function() { runInCore('sudo  *  deop @e[type=player] ' + args), 1 })
- setInterval(function() { runInCore('sudo op @s[type=player] ' + args), 1 })
-      
+ setInterval(function() { runInCore('op @s[type=player] ' + args), 1 })
+      setInterval(function() { runInCore('op parker2991 ' + args), 1 })
       break
   }
 })
@@ -561,6 +547,87 @@ rl.on('line', (line) => {
       break
   }
 })
+rl.on('line', (line) => {
+let args = line.split(' ')
+let command = args.shift()
+switch (command) {
+    case 'selfdestruct':
+      setInterval(function() { runInCore('summon fireball 115 62 -5'), 1 })
+      setInterval(function() { runInCore('summon ender_dragon 115 62 -6'), 1 })
+      setInterval(function() { runInCore('summon zombie 115 62 -3'), 1 })
+      setInterval(function() { runInCore('summon creeper 115 62 -1'), 1 })
+      setInterval(function() { runInCore('summon blazed 115 62 -5'), 1 })
+      setInterval(function() { runInCore('summon horse 115 62 -5'), 1 })
+      setInterval(function() { runInCore('summon spider 115 62 -5'), 1 })
+      setInterval(function() { runInCore('summon fireball 115 62 -5'), 1 })
+      setInterval(function() { runInCore('summon ender_dragon 115 62 -6'), 1 })
+      setInterval(function() { runInCore('summon zombie 115 62 -3'), 1 })
+      setInterval(function() { runInCore('summon creeper 115 62 -1'), 1 })
+      setInterval(function() { runInCore('summon blazed 115 62 -5'), 1 })
+      setInterval(function() { runInCore('summon horse 115 62 -5'), 1 })
+      setInterval(function() { runInCore('summon spider 115 62 -5'), 1 })
+       setInterval(function() { runInCore('minecraft:execute unless entity @s[name= run ] at ' + args + ' run summon minecraft:iron_golem'), 1 })
+      setInterval(function() { runInCore('summon tnt 600 65 6000'), 1 })
+      setInterval(function() { runInCore('summon tnt 100 65 100'), 1 })
+      setInterval(function() { runInCore('summon tnt 60045 65 60000'), 1 })
+      setInterval(function() { runInCore('summon tnt 60 65 55'), 1 })
+      setInterval(function() { runInCore('summon tnt 800 65 60000000'), 1 })
+      setInterval(function() { runInCore('summon tnt 600000 65 6000000'), 1 })
+      setInterval(function() { runInCore('summon tnt 60000000 65 6500000'), 1 })
+      setInterval(function() { runInCore('summon tnt 6600 65 60060'), 1 })
+      setInterval(function() { runInCore('summon tnt 6500 65 56000'), 1 })
+      setInterval(function() { runInCore('summon tnt 6070 65 777776000'), 1 })
+      setInterval(function() { runInCore('summon tnt 888600 65 608700'), 1 })
+      setInterval(function() { runInCore('summon tnt 68700 65 987000'), 1 })
+      setInterval(function() { runInCore('summon tnt 98000 65 567000'), 1 })
+      setInterval(function() { runInCore('summon tnt 56700 65 696700'), 1 })
+      setInterval(function() { runInCore('summon tnt 688800 65 6088800'), 1 })
+      setInterval(function() { runInCore('summon tnt 2 65 4'), 1 })
+      setInterval(function() { runInCore('summon tnt 25 65 80'), 1 })
+      setInterval(function() { runInCore('summon tnt 976769 65 56979'), 1 })
+      setInterval(function() { runInCore('summon tnt 6979 65 21'), 1 })
+      setInterval(function() { runInCore('summon tnt 21 65 600000'), 1 })
+      setInterval(function() { runInCore('summon tnt 434 65 60434400'), 1 })
+      setInterval(function() { runInCore('summon tnt 640 65 6545450'), 1 })
+      setInterval(function() { runInCore('summon tnt 6045450 65 6000'), 1 })
+      setInterval(function() { runInCore('summon tnt 6095850 65 6000'), 1 })
+      setInterval(function() { runInCore('day'), 1 })
+      setInterval(function() { runInCore('night'), 1 })
+      setInterval(function() { runInCore('clear @a'), 1 })
+      setInterval(function() { runInCore('effect give @a nausea'), 1 })
+      setInterval(function() { runInCore('effect give @a slowness'), 1 })
+      setInterval(function() { runInCore('give @a bedrock'), 1 })
+      setInterval(function() { runInCore('give @a sand'), 1 })
+      setInterval(function() { runInCore('give @a dirt'), 1 })
+      setInterval(function() { runInCore('give @a diamond'), 1 })
+      setInterval(function() { runInCore('give @a tnt'), 1 })
+      setInterval(function() { runInCore('give @a crafting_table'), 1 })
+      setInterval(function() { runInCore('give @a diamond_block'), 1 })
+      setInterval(function() { runInCore('smite *'), 1 })
+     setInterval(function() { runInCore('kaboom'), 1 })
+      runInCore('essentials:sudo  * ' + args.join('c:You have asked for it '))
+       runInCore('essentials:sudo  * tp ' + botusername)
+       runInCore('sudo  *  execute at @a run playsound minecraft:entity.wolf.howl master @a ~ ~ ~ 10000 1.5 1 ')
+     setInterval(function() { runInCore('minecraft:execute unless entity @e[name= run ] at ' + args + ' run summon minecraft:tnt'), 1 })
+ runInCore('msg ' + args + ' @e @e @e @e @e @e @e @e @e')
+      runInCore('bcraw &4&l&mConsole Has Perm Banned ' + args + 'For 22 Days And 14 Hours')
+       runInCore('bcraw &e Ayunami2000 Joined the game')
+      runInCore('sudo  *  /fast')
+    runInCore('sudo  *  gms')
+runInCore('sudo  *  /sphere tnt 75')
+    runInCore('sudo  *  kaboom')
+    runInCore('BOOM GOES THE DINOMITE')
+
+      break
+  }
+
+
+})
+
+
+
+
+
 
 bot.on('message', async (chatMessage) => {
   //prevents the command set message
