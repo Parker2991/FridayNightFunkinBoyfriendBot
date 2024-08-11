2604709b535e56f20728d7ee2df64c0068d2cefa
Parker2991 <parkerallengriffith04@gmail.com>
2024-08-10 21:58:14 -0400

    eeeeeeeee

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-function validation (bot, options, config) {
+async function validation (bot, options, config, discordClient) {
   let hashRegenInterval = setInterval(() => {
     bot.validation = {
       trusted: crypto.createHash('sha256').update(Math.floor(Date.now() / 5000) + config.keys.trusted).digest('hex').substring(0, 16),
